c57ea3180182df70ccaabab59c728edbb7edafef
Lakshan099 <lakshan.rsandanayaka@gmail.com>
2022-12-13 22:44:01 +0530

    first commit

---
+++ Characters.java
@@ -0,0 +1,16 @@
+public class Characters {
+
+    private String symWord;
+
+    public Characters(String symWord) {
+        this.symWord = symWord;
+    }
+
+    public void setWord(String symWord) {
+        this.symWord = symWord;
+    }
+
+    public String getWord() {
+        return (symWord);
+    }
+}
---
+++ Queue.java
@@ -0,0 +1,59 @@
+public class Queue {
+
+    private int maxSize;
+    private Characters queueArray[];
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public Queue(int maxSize) {
+        this.maxSize = maxSize;
+        queueArray = new Characters[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+    public boolean isFull() {
+        return (nItems == maxSize);
+    }
+
+    public void insert(String symWord) {
+        if (isFull()) {
+            System.out.println("Queue is Full");
+        } else {
+            if (rear == maxSize - 1) {
+                rear = -1;
+            }
+
+            Characters ch = new Characters(symWord);
+            queueArray[++rear] = ch;
+            nItems++;
+        }
+    }
+
+    public boolean isEmpty() {
+        return (nItems == 0);
+    }
+
+    public Characters remove() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            if (front == maxSize) {
+                front = 0;
+            }
+            nItems--;
+            return queueArray[front++];
+        }
+    }
+
+    public Characters peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return queueArray[front];
+        }
+    }
+
+}
